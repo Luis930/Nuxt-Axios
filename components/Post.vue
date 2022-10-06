d450e2f74b4a80dd94d7d6f8ba26ca2a6313193f
Luis930 <guedez.luis90@gmail.com>
2022-10-06 17:02:53 -0400

    Proyecto terminado

---
+++ components/Post.vue
@@ -0,0 +1,33 @@
+<template>
+
+        <div class="post">
+        <p>{{id}}. {{name}}</p>
+        </div>
+
+
+</template>
+
+<script>
+export default {
+  name:'Post',
+  props:['id','name']
+}
+</script>
+
+<style>
+  .post{
+    padding: 1rem;
+    border: 1px solid whitesmoke;
+    margin: 1rem 0;
+  }
+
+  .post:hover{
+    background: #2d3436;
+    border: 1px solid #6c5ce7;
+    cursor:pointer
+  }
+
+  a{
+
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1 class='m3'>Posts</h1>
+    <Post
+    v-for="post of posts"
+    :key="post.id"
+    :name='post.title'
+    :id='post.id' />
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Post from '../components/Post.vue'
+
+export default {
+  components:{
+    Post
+  },
+  name: 'IndexPage',
+  head(){
+    return{
+    title:'App Nuxt',
+    meta:[{
+      hid:'Description',
+      name:'Description',
+      content:'Website'
+    }],
+    htmlAttrs:{
+      lang:'en'
+    }
+    }
+  },
+  data(){
+    return{
+      posts:[]
+    }
+  },
+  async created(){
+      try {
+        const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        this.posts = res.data
+        console.log(res)
+      } catch (error) {
+        console.log(error)
+      }
+  }
+}
+</script>
